6b71d6b9623f3f1e975b162f8f825de803a9f30f
sidharth132 <55968842+sidharth132@users.noreply.github.com>
2025-12-02 00:03:29 +0530

    Update TodoApp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,7 +4,7 @@ import { Button, TextField, Container, Typography, Grid, Card, CardContent, Icon
 import { Delete } from '@mui/icons-material';
 import { Box } from '@mui/material';
 
-const API_BASE_URL = 'http://10.0.171.234:8000';
+const API_BASE_URL = 'http://localhost:8000';
 
 const backgroundImage = process.env.PUBLIC_URL + '/background.jpg';
 
