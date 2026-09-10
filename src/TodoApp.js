a91ae5a46f5cbb3089cad454e0872af1721f72aa
sidharth132 <55968842+sidharth132@users.noreply.github.com>
2026-09-10 23:53:09 +0530

    Update TodoApp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,7 +4,7 @@ import { Button, TextField, Container, Typography, Grid, Card, CardContent, Icon
 import { Delete } from '@mui/icons-material';
 import { Box } from '@mui/material';
 
-const API_BASE_URL = "http://135.119.65.152:8000/app";
+const API_BASE_URL = "http://135.119.65.152:8000/api";
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 console.log("API:", process.env.REACT_APP_API_BASE_URL);
 // console.log("API BASE URL:", API_BASE_URL);
